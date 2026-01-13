76bd982f94952ce4e6be057f69474a905acefadf
VoltDev001 <tirthp0703@gmail.com>
2026-01-14 01:02:08 +0530

    added comments

--- pattern_space_minus_star.c
+++ pattern_space_minus_star.c
@@ -1,10 +1,10 @@
 #include<stdio.h>
 int main(){
     for(int i=1; i<=4; i++){
-        for(int k=4; k>i; k--){
+        for(int k=4; k>i; k--){ // or (int k=1; k<=(4-i;); k++)
             printf(" ");
         }
-        for(int j=1; j<=i; j++){
+        for(int j=1; j<=i; j++){  // or (int j=1; j<=i; j++)
             printf("*");
         }
         printf("\n");
